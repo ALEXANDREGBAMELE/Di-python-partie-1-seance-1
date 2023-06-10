4198b7b8a01430a06d2f0a8a9d7319cd1ff6804b
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-06-10 02:30:19 +0200

    first commit exo

---
+++ exercice1.py
@@ -0,0 +1,78 @@
+# # first variable
+# first = 'hello world'
+# # this is a comment 
+# print("I AM A COMPUTER!")
+
+# # Write an if statement that checks if 1 is less than 2 and if 4 is greater than 2. 
+# # If it is, show the message "Math is fun."
+
+# if (1<2 and 4>2):
+#     print("Math is fun.")
+    
+# # Assign a variable called nope to an absence of value.
+# nope = "frais"
+# nope  = bool(nope)
+# print(nope)
+# # Calculate the length of the string "What's my length?"
+# text = "What's my length?"
+# text_len = len(text)
+# print(text_len)
+# # Convert the string "i am shouting" to uppercase.
+# text_upper = text.upper()
+# print(text_upper)
+
+# # Convert the string "1000"to the number 1000.
+# number = "1000"
+# number = int(number)
+# print(number)
+
+# # Combine the number 4 with the string "real" to produce "4real".
+# four = 4
+# real = "real"
+# four = str(four)
+# print(four + real)
+
+# # Enregistrez la sortie de l'expression 3 * "cool".
+# falsemul_tiplication = 3 * "cool"
+# print(falsemul_tiplication)
+
+# # Enregistrez la sortie de l'expression 1 / 0.
+# # ------division = 1/0
+# # ------print(division)
+
+# # Determine the type of [].
+# array = []
+# print(type(array))
+
+# # Ask the user for their name, and store it in a variable called name.
+# name = input("Enter your name : ")
+# print("My name is " + name)
+
+# ''' Ask the user for a number. If the number is negative, show a message that says "That number is less than 0!" 
+#     If the number is positive, show a message that says "That number is greater than 0!" 
+#     Otherwise, show a message that says "You picked 0!.
+# '''
+
+# number = input("Enter a number : ")
+# number = int(number)
+# if number < 0 :
+#     print("That number is less than 0!" )
+# elif number > 0 :
+#     print("That number is greater than 0!")
+# else : print("You picked 0")
+
+# # Find the index of "l" in "apple".
+# apl = "apple"
+# print(apl.index("l"))
+
+# # Check whether "y" is in "xylophone".
+# letter = "y"
+# word = "xylophone"
+# if letter in word:
+#     print(letter)
+    
+# Check whether a string called my_string is all in lowercase.
+my_string = "my_string"
+for item in my_string :
+    if item.isnolower :
+        print(item)
